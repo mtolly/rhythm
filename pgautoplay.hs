a4bba67076511e4f47fb5d6d15c22c903a96446f
Michael Tolly <tolly@wisc.edu>
2012-08-14 19:36:05 -0500

    Pro gtr autoplayer and documentation cleaned up

--- pgautoplay.hs
+++ pgautoplay.hs
@@ -3,12 +3,12 @@ module Main where
 -- pro guitar autoplayer
 -- example app using rhythm and midiproadapter libraries
 
-import qualified Data.Rhythm.RockBand.Lex.ProGuitar as PG
-import Data.Rhythm.RockBand.Common
-
 import Data.Rhythm.Event
 import Data.Rhythm.Interpret
 import Data.Rhythm.Time
+import qualified Data.Rhythm.MIDI as MIDI
+import Data.Rhythm.RockBand.Common
+import qualified Data.Rhythm.RockBand.Lex.ProGuitar as PG
 
 import qualified Data.RockBand.MIDIProAdapter as MPA
 
@@ -21,11 +21,10 @@ import qualified Sound.MIDI.File.Event.SystemExclusive as SysEx
 import qualified Sound.MIDI.File.Load as Load
 import qualified Sound.MIDI.File.Save as Save
 
-import qualified Data.Rhythm.MIDI as MIDI
-
-import Debug.Trace
-
 import System.Environment (getArgs)
+import Data.Char (toUpper)
+import Control.Applicative ((<|>))
+import Data.Maybe (fromMaybe)
 
 autoplayEvent :: PG.DiffEvent -> [MPA.GtrMessage]
 autoplayEvent (PG.Note str frt typ) = case typ of
@@ -44,44 +43,39 @@ getDiffEvents d = RTB.mapMaybe $ \x -> case x of
   Length _ (PG.DiffEvent d' evt) | d == d' -> Just evt
   _ -> Nothing
 
-squier :: RTB.T t MPA.GtrMessage -> RTB.T t MPA.GtrEvent
-squier = fmap $ MPA.GtrEvent MPA.Squier
-
 toMIDI :: RTB.T t MPA.GtrEvent -> RTB.T t E.T
 toMIDI = fmap $ E.SystemExclusive . SysEx.Regular . (++ [0xF7]) . MPA.fromGtrEvent
 
 -------------
 
-pgToMIDI :: (NN.C t) => Difficulty -> RTB.T t (PG.T t) -> RTB.T t E.T
-pgToMIDI d = toMIDI . squier . autoplay . getDiffEvents d
+pgToMIDI :: (NN.C t) => MPA.GtrController -> Difficulty -> RTB.T t (PG.T t) -> RTB.T t E.T
+pgToMIDI c d = toMIDI . fmap (MPA.GtrEvent c) . autoplay . getDiffEvents d
 
-data PGTrack = PG | PB | PG22 | PB22
+data Instrument = Guitar | Bass
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
-getPG :: PGTrack -> MIDI.File Bool -> RTB.T Beats (PG.T Beats)
-getPG pgt mid = case MIDI.getTrack name mid of
-  Nothing -> RTB.empty
-  Just trk -> case interpretRTB PG.interpret $ toUnified trk of
-    (pg, _, _) -> pg
-  where name = case pgt of
-          PG -> "PART REAL_GUITAR"
-          PB -> "PART REAL_BASS"
-          PG22 -> "PART REAL_GUITAR_22"
-          PB22 -> "PART REAL_BASS_22"
+getPG :: MPA.GtrController -> Instrument -> MIDI.File Bool -> RTB.T Beats (PG.T Beats)
+getPG c i mid = go $ fromMaybe (error "MIDI track not found") $ case c of
+  MPA.Squier -> MIDI.getTrack name22 mid <|> MIDI.getTrack name17 mid
+  MPA.Mustang -> MIDI.getTrack name17 mid
+  where go t = case interpretRTB PG.interpret $ toUnified t of (pg, _, _) -> pg
+        name17 = "PART REAL_" ++ map toUpper (show i)
+        name22 = name17 ++ "_22"
 
-run :: PGTrack -> Difficulty -> FilePath -> FilePath -> IO ()
-run pgt d fin fout = Load.fromFile fin >>= \f -> case MIDI.standardFile f of
+run :: MPA.GtrController -> Instrument -> Difficulty -> FilePath -> FilePath -> IO ()
+run c i d fin fout = Load.fromFile fin >>= \f -> case MIDI.standardFile f of
   Nothing -> putStrLn "Not a type-1 ticks-based MIDI file"
   Just m -> let
-    auto = fmap MIDI.standardEvent $ pgToMIDI d $ getPG pgt m
+    auto = fmap MIDI.standardEvent $ pgToMIDI c d $ getPG c i m
     m' = m { MIDI.tracks = [(Just "Autoplay", auto)] }
     in case MIDI.rawFile m' of
       Left _ -> putStrLn "time signature error"
       Right f -> Save.toFile fout f
 
 main = getArgs >>= \argv -> case argv of
-  [pgt, d, fin, fout] -> run (read pgt) (read d) fin fout
+  [c, i, d, fin, fout] -> run (read c) (read i) (read d) fin fout
   _ -> mapM_ putStrLn
-    [ "Usage: pgautoplay track diff file-in file-out"
-    , "track is one of: PG22 PB22"
+    [ "Usage: pgautoplay cont inst diff file-in file-out"
+    , "cont is one of: Squier Mustang"
+    , "inst is one of: Guitar Bass"
     , "diff is one of: Easy Medium Hard Expert" ]
--- src/Data/Rhythm/Interpret.hs
+++ src/Data/Rhythm/Interpret.hs
@@ -1,11 +1,7 @@
 {- |
-The rhythm library is built around data formats which can be viewed at one of
-several layers of structure, and which often have only partial public
-documentation. This module provides a common interface to the task of parsing
-a general format into a more structured one, while possibly generating warnings
-or errors.
+A common interface for parsing from a general type to a specific one, and
+vice versa.
 -}
-{-# LANGUAGE MultiParamTypeClasses #-}
 module Data.Rhythm.Interpret where
 
 import qualified Data.EventList.Relative.TimeBody as RTB
@@ -13,6 +9,8 @@ import qualified Numeric.NonNegative.Class as NN
 import Control.Monad.Trans.Writer
 import Control.Monad.Trans.Class
 
+-- | General, raw type -> specific, strict type. Unrecognized input returns
+-- Nothing. Recognized input can also generate warnings using the Writer monad.
 type Interpreter a b = a -> WriterT [String] Maybe [b]
 
 interpretRTB :: (NN.C t) => Interpreter a b ->
@@ -31,8 +29,9 @@ single x = return [x]
 none :: WriterT [w] Maybe a
 none = lift Nothing
 
+-- | Specific, strict type -> general, raw type. No errors/warnings because
+-- the conversion always succeeds.
 type Uninterpreter a b = a -> [b]
 
 uninterpretRTB :: (NN.C t) => Uninterpreter a b -> RTB.T t a -> RTB.T t b
 uninterpretRTB f = RTB.flatten . fmap f
-
